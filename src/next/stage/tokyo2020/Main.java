475a0dc8cd993fc9d2062fd4d5c413d24401d944
Nicolas Raoul <nicolas.raoul@gmail.com>
2014-11-08 01:38:50 +0900

    Initial proof of concept

---
+++ src/next/stage/tokyo2020/Main.java
@@ -0,0 +1,25 @@
+package next.stage.tokyo2020;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Main extends Activity {
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        WebView wv = (WebView)findViewById(R.id.my_webview);
+        wv.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        wv.loadUrl("file:///android_asset/Odaiba.html");
+    }
+}
